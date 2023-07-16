7169534df60d12db02638c954fa90ba7adb8d41d
realasyed <armaansyed120@gmail.com>
2023-07-16 11:10:05 -0700

    basic website layout

--- let-me-cook/src/app/page.tsx
+++ let-me-cook/src/app/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export default function Home() {
+const Page = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="max-w-md bg-white p-8 shadow-lg rounded-lg">
+        <h1 className="text-3xl mb-4 text-gray-700">Stupid webite</h1>
+        <p className="text-gray-700">
+          lol
+        </p>
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md">
+          this does nothing
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
