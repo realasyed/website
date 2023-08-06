b37124f9b9b3c852ba7a321bc1b27b8bd551cbc2
realasyed <armaansyed120@gmail.com>
2023-08-06 13:07:14 -0500

    stuff

--- let-me-cook/src/app/philosophy/philosophy.tsx
+++ let-me-cook/src/app/philosophy/philosophy.tsx
@@ -3,22 +3,8 @@ import Link from "next/link";
 
 const Philosophy: React.FC = () => {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <header className="bg-blue-500 p-4 flex justify-between items-center">
-        <h1 className="text-white text-xl font-bold">
-          Armaan's Cool Placeholder
-        </h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <Link href="let-me-cook/src/app/philosophy/philosophy">
-                Philosophy
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main className="p-4">{}</main>
+    <div className="idk">
+      <h2>FINALLY!!!!!</h2>
     </div>
   );
 };
