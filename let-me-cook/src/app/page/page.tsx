1ef94880d56ddcdc2f3d5aca515f0297f08fe74b
realasyed <armaansyed120@gmail.com>
2023-08-06 12:47:09 -0500

    helpppp

--- let-me-cook/src/app/page/page.tsx
+++ let-me-cook/src/app/page/page.tsx
@@ -11,7 +11,7 @@ const Page: React.FC = () => {
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link href="./philosophy">Philosophy</Link>
+              <Link href="/philosophy">Philosophy</Link>
             </li>
           </ul>
         </nav>
